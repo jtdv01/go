a6f9b9137637b6a46a626baab27956170dcd43cd
j <j@j.com>
2016-04-17 19:35:58 +1000

    Init

---
+++ hello-world.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello world!")
+
+	// Static typing, once defined cannot be changed
+
+	// Int 
+	var age int =40;
+	fmt.Println(age);
+
+	// Float
+	var fl float64= 1.654;
+	fmt.Println("This is a float ",fl);
+
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # Go
 
 My reference for Go programming language
+
+# References
+Derek Banas
+https://www.youtube.com/watch?v=CF9S4QZuV30
+
+
