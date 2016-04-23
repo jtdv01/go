4ea83fce3fd281e786d29386e4f618a1e58c0b07
j <j@j.com>
2016-04-23 18:39:42 +1000

    Added channels

---
+++ concurrency/channels.go
@@ -0,0 +1,31 @@
+package main
+import (
+  "fmt"
+  "time"
+  "math/rand"
+)
+func main() {
+  c := make(chan int)
+  for i := 0; i < 5; i++ {
+      worker := &Worker{id: i}
+      go worker.process(c)
+  }
+
+  for j:=0; j<20; j++{
+    data := rand.Int()
+    fmt.Printf("New job came! Job id: %d ; Data: %d\n",j,data)
+    c <- data 
+    time.Sleep(time.Millisecond * 50)
+  }
+}
+
+type Worker struct {
+  id int
+}
+
+func (w *Worker) process(c chan int) {
+  for {
+    data := <-c
+    fmt.Printf("worker %d got %d\n", w.id, data)
+  }
+}
